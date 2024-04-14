bd2f3a25c12e53cb597144348110309474061af5
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-04-14 10:55:04 -0300

    Pastas atualizadas

--- projeto-java/VARIAVEIS/src/App.java
+++ projeto-java/VARIAVEIS/src/App.java
@@ -1,7 +1,53 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+
+//       //Variáveis do tipo inteiro
+    
+//       byte numeroPequeno = 127;
+//       short numeroShort = 32767;
+//       int numeroMaior = 2147483647;
+//       long numeroEnorme = 922337203685477000l;
+
+//       //Variáveis do tipo Ponto Flutuante
+
+//       float peso = 78.7f;
+//       double pi = 3.14123458796521477854;
+
+//       //Vriáveis do tipo caractere
+
+//       char letra = 'L';
+
+//       //Variável boleano
+
+//       boolean estouComFome = false;
+
+//       // Varável do tipo String
+
+//       String nome = "Lucas";
+//       String texto = "Olá, bom dia!";
+
+//       System.out.println(numeroPequeno); 
+//       System.out.println(numeroShort);
+//       System.out.println(numeroMaior);
+//       System.out.println(numeroEnorme);
+//       System.out.println(texto);
+     
+//     }
+// }
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        // tipo nome
-        byte numeroPequeno = 125;
-        System.out.println(numeroPequeno);
-    }
+      public static void main(String[] args) throws Exception {
+      String nomeCompleto = "Naruto Uzumaki";
+      int idade = 25;
+      int dataDeNascimento = 1998;
+      float peso = 65.8f;
+      char primeiraLetraDoNome = "N";
+      boolean eleFoiDerrotado = false;
+
+      System.out.println(nomeCompleto);
+      System.out.println(idade);
+      System.out.println(dataDeNascimento);
+      System.out.println(peso);
+      System.out.println(primeiraLetraDoNome);
+  }
 }
