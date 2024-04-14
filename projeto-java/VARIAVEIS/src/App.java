5e43af7522c6c8c8ca9b70b34658b1d5c66ee5b4
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-04-14 09:35:26 -0300

    Pastas adicionadas

---
+++ projeto-java/VARIAVEIS/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // tipo nome
+        byte numeroPequeno = 125;
+        System.out.println(numeroPequeno);
+    }
+}
---
+++ projeto-java/VARIAVEIS2/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+      byte numeroPequeno = 127;
+      short numeroShort = 32767;
+      int numeroMaior = 2147483647;
+      long numeroEnorme = 922337203685477000l;
+
+      System.out.println(numeroPequeno); 
+      System.out.println(numeroShort);
+      System.out.println(numeroMaior);
+      System.out.println(numeroEnorme);
+     
+    }
+}
